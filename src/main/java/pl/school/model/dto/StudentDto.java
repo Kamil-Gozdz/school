14345b195500a42ae8c5425c67f00157e3bb16b6
Kamil <kamilgozdz23@gmail.com>
2022-11-21 19:06:27 +0100

    feature/Adding_validation

--- src/main/java/pl/school/model/dto/StudentDto.java
+++ src/main/java/pl/school/model/dto/StudentDto.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Getter
@@ -13,14 +16,15 @@ import java.util.Set;
 @Builder
 public class StudentDto {
 
-    private Long id;
 
+    private Long id;
+    @Size(min = 3, message = "Name must be longer than two characters")
     private String name;
 
     private String surname;
-
+    @Email(message = "Email is  incorrect")
     private String email;
-
+    @Min(value = 19, message = "Age can't be less than 19")
     private Integer age;
 
     private Set<TeacherDto> teachers;
